f2e26a99fd30e5a9bc5f90cf490579b662fddbcb
Aki Blasco <inaki.blasco1@gmail.com>
2024-07-17 19:16:10 -0700

    removed comments I left throughout the coding of the project so I can put it in the README file of the repo.

--- src/App.js
+++ src/App.js
@@ -57,33 +57,3 @@ export default App;
 
 
 
-/* 
-The moment I started working on this part, is when I had to do most of the research. The amount of work it took
-for me to unrust the limited JavaScript knowledge I had felt horrible. Luckily, there is a lot of documentation
-on JavaScript, so I was able to reverse engineer some of the functions posted in various problems to implement in my code.
-I also used co-pilot for minor debugging for the first itteration, since this is not my main programming language,
-and syntax and formatting was taking me a long time.
-*/
-
-/*
-First go at it was catastrophical with multiple issues found and errors. I will have to look at what I did wrong.
-I get multiple uncaught reuntime errors (type errors) Saying it is expecting a string, but is getting an obejct,
-and I do not know why is that. I tried fixing it but it did not seem to resolve this issue.
-*/
-
-/*
-Panic and despair are starting to get to me as I make no progress with not even getting the button to show up, and my screen
-turning red every time I click the new submission button. I think I need to familiarize myself with the code and environment a bit
-more before I can try the implementation again. Will try to finish it as I get home, but because not having touched frontend, and especially
-JavaSCript and react for years, I do not know if I will get it to work. There seems to be a type mismatch, I just do not understand where.
-*/
-
-/*
-I ran npm test and said the test is passing but for some reason I am still getting errors in the local host server and it still is the type issue.
-*/
-
-/*
-I am having to search a lot of the functions and syntax to verify I am doing it correctly, however, I do feel I am making progress. To be honest, there
-is a gap between my Programming knowledge and logic that seems to be very sound and crucial for the understanding of the problem, and JavaScript, which is where
-I am having the most trouble.
-*/
